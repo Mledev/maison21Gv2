4abd931a25f4e4c3968877ff23e95fdee3268d14
Matthieu Le Dévéhat <mledevehat@voxsquare.com>
2019-06-11 12:03:24 +0200

    addVideo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './assets/images/logo.svg';
+import bgVideo from './assets/video/home.mp4';
 import './assets/styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="bg-video-container">
+          <video className="bg-video" autoPlay loop src={bgVideo}></video>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
