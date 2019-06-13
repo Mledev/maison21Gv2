3859d1bdde84b7bd34338db9dc92f8e5c1adea5e
Matthieu Le Dévéhat <mledevehat@voxsquare.com>
2019-06-13 18:00:30 +0200

    build version

---
+++ build/precache-manifest.3c55b60d8825703de71fde396f20e352.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06480792f6f39c542efaaa3c2422934c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b08da79f37dd3426c2e",
+    "url": "/static/css/main.dde2d852.chunk.css"
+  },
+  {
+    "revision": "a756d753a45d1a63ce4f",
+    "url": "/static/js/2.910d52a6.chunk.js"
+  },
+  {
+    "revision": "7b08da79f37dd3426c2e",
+    "url": "/static/js/main.b89e5750.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "099d5a64e81bb172f2fcd01c9d6a3172",
+    "url": "/static/media/capsule.099d5a64.png"
+  },
+  {
+    "revision": "d8340c4e0497d261400345f938087c6d",
+    "url": "/static/media/facebook.d8340c4e.svg"
+  },
+  {
+    "revision": "1493f9cf65d6af40a67a30a7a9c1b756",
+    "url": "/static/media/fille.1493f9cf.png"
+  },
+  {
+    "revision": "50fcdbeec41660efc143fbfabbc8fb4f",
+    "url": "/static/media/firstFlacon.50fcdbee.png"
+  },
+  {
+    "revision": "e1d5c9e74a2e1494fe13009f6acc8e88",
+    "url": "/static/media/firstbg.e1d5c9e7.png"
+  },
+  {
+    "revision": "383b3aa87da77d666b5c5cd37f52596a",
+    "url": "/static/media/flacon.383b3aa8.png"
+  },
+  {
+    "revision": "05a2ab67d29ce8a60ed77852c37afe3d",
+    "url": "/static/media/instagram.05a2ab67.svg"
+  },
+  {
+    "revision": "e7dbf72e53c41ea11a67c78db0ffb05c",
+    "url": "/static/media/lastbg.e7dbf72e.png"
+  },
+  {
+    "revision": "548e82e96cb75eb6e05801117876613f",
+    "url": "/static/media/lavande.548e82e9.png"
+  },
+  {
+    "revision": "99ab5394dc08a67d6f4a6a09817c18c0",
+    "url": "/static/media/logo.99ab5394.svg"
+  },
+  {
+    "revision": "9971eebc8836e5328cc0f7853256f639",
+    "url": "/static/media/mandarine.9971eebc.png"
+  },
+  {
+    "revision": "7b5ccc0e86bf3a39c6d9899fe2133ad5",
+    "url": "/static/media/phone.7b5ccc0e.png"
+  },
+  {
+    "revision": "e761730b6d045157c0de120b2b010232",
+    "url": "/static/media/troisparfums.e761730b.png"
+  },
+  {
+    "revision": "a8b2b2b51e3e32ad8abc2730bbe0bf1b",
+    "url": "/static/media/twitter.a8b2b2b5.svg"
+  }
+]);
