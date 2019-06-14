9bba5fa137f2b635876fadcffa7b16fbcfb091d4
Matthieu Le Dévéhat <mledevehat@voxsquare.com>
2019-06-14 09:03:34 +0200

    fix video

--- src/component/fourthcontent.js
+++ src/component/fourthcontent.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import video from '../assets/video/prez.mov'
 
 function FourthContent(){
     return(
         <div className="fourth-content">
             <div className="fourth-content-text">
                 <h2>Je mixe mes fragrances</h2>
-                <video className="video-react-video" loop preload="true" autoPlay src="https://s.scentdesigner.stage.23c.se/media/documents/Comp_2.mov"></video>
+                <video className="video-react-video" loop preload="true" autoPlay src={video}></video>
             </div>
         </div>
     )
