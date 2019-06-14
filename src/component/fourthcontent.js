88624cb972113ebc5c3b5ef1ebf7cd98b4e9ee5d
Matthieu Le Dévéhat <mledevehat@voxsquare.com>
2019-06-14 09:58:57 +0200

    fix video 4

--- src/component/fourthcontent.js
+++ src/component/fourthcontent.js
@@ -1,16 +1,25 @@
 import React, {Component} from 'react';
 import video from '../assets/video/prez.mp4'
+//import "node_modules/video-react/dist/video-react.css"; // import css
+import { Player, ControlBar } from 'video-react';
+
 
 class FourthContent extends Component{
     constructor(props){
         super(props)
 
         this.state = {
+            video: null
         }
     }
 
     componentDidMount(){
-        document.querySelector('video').play();
+        var media = document.getElementById("video");
+        media.load();
+
+        media.addEventListener('loadeddata', function() {
+            media.play();
+        });
     }
 
     render(){
@@ -18,7 +27,7 @@ class FourthContent extends Component{
             <div className="fourth-content">
                 <div className="fourth-content-text">
                     <h2>Je mixe mes fragrances</h2>
-                    <video className="video-react-video" loop preload="auto" autoPlay src={video}></video>
+                    <video id="video" className="video-react-video" muted loop preload="auto" src={video}></video>
                 </div>
             </div>
         )
